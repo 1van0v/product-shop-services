e5acb742a85cea768d2d6cc96f14a39e1d07f0f1
1van0v <ivanov5oleksandr@gmail.com>
2021-09-27 18:27:56 +0300

    task 5.1

---
+++ import-service/handlers/file-uploader.js
@@ -0,0 +1,28 @@
+import { BUCKET } from '../constants';
+import { s3 } from '../services/s3.service';
+
+export const importProductsFile = async (event) => {
+  const fileName = event.queryStringParameters.name;
+  const uploadPath = `uploaded/${fileName}`;
+  const uploadParameters = {
+    Bucket: BUCKET,
+    Key: uploadPath,
+    Expires: 60,
+    ContentType: 'text/csv'
+  };
+
+  return new Promise((resolve, reject) => {
+    s3.getSignedUrl('putObject', uploadParameters, (error, url) => {
+      if (error) {
+        console.log('error', error);
+        return reject(error);
+      }
+
+      resolve({
+        statusCode: 202,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: url
+      });
+    });
+  });
+}
