b643fb89a2912e49df1635e7ed0279a85f907e32
1van0v <ivanov5oleksandr@gmail.com>
2021-10-06 01:58:51 +0300

    feat: add sns

--- product-service/handlers/batch-processor.js
+++ product-service/handlers/batch-processor.js
@@ -1,3 +1,4 @@
+import { SNS } from 'aws-sdk';
 import * as productService from '../services/products.service';
 import { createJsonResponse } from '../utils/create-json-response';
 
@@ -12,9 +13,19 @@ export const catalogBatchProcess = async ({ Records }) => {
       await productService.createProduct(product);
     }
 
+    const sns = new SNS({ region: 'eu-west-1' });
+
+    const isSent = await sns.publish({
+      Subject: 'Bulk product creation',
+      Message: `Created ${Records.length} products.`,
+      TopicArn: process.env.SNS_ARN
+    }).promise()
+
+    console.log('email was sent', isSent);
     console.log('processed', Records.length);
     return createJsonResponse({});
   } catch (e) {
+    console.log('batch processor error', e);
     return e;
   }
 }
