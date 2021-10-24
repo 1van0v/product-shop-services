6aa9716e866efb23e9f4fafcc5fd8d52df4ce848
1van0v <ivanov5oleksandr@gmail.com>
2021-10-24 23:27:42 +0300

    task-7 (#5)
    * task-7
    
    * feat: add authorizer
    
    * fix review comments
    
    * review fixes

---
+++ authorization-service/handler.js
@@ -0,0 +1,38 @@
+const isAllowed = (username, password) => {
+  return process.env?.[username] === password;
+};
+
+export const basicAuthorizer = (event, context, callback) => {
+  if (event.type !== 'REQUEST') {
+    callback('Unauthorized');
+  }
+
+  try {
+    const credentials = event.headers.authorization.split(' ')[1];
+    const [username, password] = Buffer.from(credentials, 'base64').toString('utf-8').split(':');
+    const effect = isAllowed(username, password) ? 'Allow' : 'Deny';
+
+    console.log(effect, 'access for', username);
+    console.log('event', JSON.stringify(event));
+    const policy = {
+      principalId: username,
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: effect,
+            Resource: event.routeArn
+          }
+        ]
+      }
+    };
+
+    console.log('policy', JSON.stringify(policy));
+
+    return callback(null, policy)
+  } catch (e) {
+    console.log('error', e);
+    callback(`Unauthorized: ${e.message}`);
+  }
+};
